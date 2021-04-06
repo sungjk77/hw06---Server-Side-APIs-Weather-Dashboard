fee433a5a65a91101e639314aa62d3a1ee82dea2
Sung Kim <sungjk77@msn.com>
2021-04-05 17:13:35 -0700

    updated readme and added a screenshot

--- README.md
+++ README.md
@@ -11,7 +11,7 @@ last modified: 4/4/2021 @ 10:24 AM PST
 
 ------------
 ## 💻 _**index.html**_
-- Created the template using Bootstrap
+- Created the template using Bootstrap, attempted to make it look like the demo
 
 ------------
 ## 🎨 _**style.css**_
@@ -19,6 +19,13 @@ last modified: 4/4/2021 @ 10:24 AM PST
 
 ------------
 ## 📝 _**script.js**_
+- created a function to read an API to get location data
+- created a function to read the weather and forecast information
+- assigned to a variable and pushed the information into an array
+- function to display the weather data created
+- function to search by city created
+- saved city information in local storage
+- added the UV rating scale
 
 ------------
 ## 📸 _**Screen Shot:**_
--- assets/js/script.js
+++ assets/js/script.js
@@ -5,7 +5,7 @@ var APIkey = '23b89ccd37ecfa0524d0c35eae3690f8';
 var weather = [];
 var cityList = [];
 
-function displayWeather(weather) {
+function displayWeather(weather) {  //updates the weather information for the various IDs
     $("#day0-wind").text(weather[0].wind);
     $("#day0-UV").text(weather[0].UV);
     $(`#currentCityName`).text(currentCityName);
@@ -15,16 +15,16 @@ function displayWeather(weather) {
     if (weather[0].UV < 6) {varUV = `Yellow`}; // Moderate
     if (weather[0].UV < 3) {varUV = `Green`}; // Low
     $(`#day0-UV`).css( "background-color", varUV);
-
     for (var i = 0; i <= 5; i++) {
-        $(`#day`+i+`-temp`).text(`Temp: `+weather[i].temp);
+        $(`#day`+i+`-temp`).text(weather[i].temp);
         $(`#day`+i+`-icon`).html(weather[i].icon);
-        $(`#day`+i+`-hum`).text(`Humidity: `+ weather[i].hum);
+        $(`#day`+i+`-hum`).text(weather[i].hum);
         $(`#day`+i+`-date`).html(weather[i].date);
         $(`#day`+i+`-icon`).attr("src",weather[i].icon);
     }
 }
-function showCityList(cityList) {
+
+function showCityList(cityList) {  //displays the list of cities chosen in the past
     var varText = "";
     for (var i = 0; i < cityList.length; i++) {
       varText += `<li class="btn list-group-item list-group-item-action d-flex justify-content-between align-items-center" onclick="searchApi('`+cityList[i]+`')">`+cityList[i]+`</li>`;
@@ -32,15 +32,14 @@ function showCityList(cityList) {
     $(`#cityListGroup`).html(varText);
 }
 
-function updateCityList(currentCityName) {
+function updateCityList(currentCityName) {  //saves the city list to local storage
     cityList.indexOf(currentCityName) === -1 ? cityList.push(currentCityName) : console.log("City already on list")
     localStorage.setItem("cityList", JSON.stringify(cityList)); //saves cityList
     showCityList(cityList);
 }
 
-function searchApi2(varLat, varLon, currentCityName) {
+function searchApi2(varLat, varLon, currentCityName) {  //have to search onecal API to get 5 day forcast 
     var locQueryUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=`+varLat+`&lon=`+varLon+`&exclude=hourly&units=imperial&appid=23b89ccd37ecfa0524d0c35eae3690f8`;
-    
     fetch(locQueryUrl)
       .then(function (response) {
         console.log(response)
@@ -51,8 +50,6 @@ function searchApi2(varLat, varLon, currentCityName) {
         return response.json();
       })
       .then(function (locRes) {
-         // write query to page so user knows what they are viewing
-        // resultTextEl.textContent = locRes.city.name;
         console.log(locRes);
         weather = [];
         updateCityList(currentCityName);
@@ -65,30 +62,16 @@ function searchApi2(varLat, varLon, currentCityName) {
                  "UV":locRes.daily[i].uvi,
                  "icon":`http://openweathermap.org/img/wn/`+locRes.daily[i].weather[0].icon+`.png`
              }
-        //    wDay.temp=kelvinToFahr(wDay.temp);
         wDay.date=wDay.date * 1000;
         const dateObject = new Date(wDay.date);
         wDay.date=dateObject.toLocaleDateString();
         weather.push(wDay);
         }   
-
-        console.log(weather);
         displayWeather(weather);
-  
-        // if (!locRes.results.length) {
-        //   console.log('No results found!');
-        //   resultContentEl.innerHTML = '<h3>No results found, search again!</h3>';
-        // } else {
-        //   resultContentEl.textContent = '';
-        //   for (var i = 0; i < locRes.results.length; i++) {
-        //     printResults(locRes.results[i]);
-        //   }
-        // }
       })
       .catch(function (error) {
         console.error(error);
       });
-
   }
 
 function searchApi(query) {
@@ -96,11 +79,13 @@ function searchApi(query) {
  
     fetch(locQueryUrl)
       .then(function (response) {
-        console.log(response)
+        console.log(response);
         if (!response.ok) {
+          $("#search-input")[0].reset()
+          alert("ERROR: City not found");
           throw response.json();
         }
-        console.log(response.json)
+        console.log(response.json);
         return response.json();
       })
       .then(function (locRes) {
@@ -113,7 +98,6 @@ function searchApi(query) {
       .catch(function (error) {
         console.error(error);
       });
-
 }
 
 function handleSearchFormSubmit(event) {
@@ -139,4 +123,3 @@ searchFormEl.addEventListener('submit', handleSearchFormSubmit);
 
 cityList = loadCityList(cityList);
 searchApi("Seattle");
-
